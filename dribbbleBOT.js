41faca902d17a94583a05bcbf8961739d08bd22f
r8w9 <28980824+r8w9a5k@users.noreply.github.com>
2019-07-02 17:59:58 +0200

    Update dribbbleBOT.js

--- dribbbleBOT.js
+++ dribbbleBOT.js
@@ -1,28 +1,24 @@
-javascript:
+function run() {
+var like = document.querySelector(".like-shot");
+var next = document.querySelector(".shot-nav-next a");
 
-function run(){ //function run wraps all the code into one run() function
-var like = document.querySelector(".like-shot"); //select like button
-var next = document.querySelector(".shot-nav-next a"); //select next page
+  function simulateClick (elem) {
+    var evt = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window 
+      });
+      var canceled = !elem.dispatchEvent(evt);
+      };
 
-	function simulateClick (elem) { 		     //core of simulate click function
-		var evt = new MouseEvent('click', {
-			bubbles: true,
-			cancelable: true,
-			view: window 
-		});
-		var canceled = !elem.dispatchEvent(evt); // end of core simulate click function
-	};
+      if (!like.classList.contains("current-user-likes")){
+	simulateClick(like);
+	simulateClick(next);
+      }
+      else {
+	simulateClick(next);
+      }
 
-		if (!like.classList.contains('current-user-likes')){
-			simulateClick(like);	//then like
-			simulateClick(next);	//&& go next
-		}
-		else {				//else don't like anything and go next
-			simulateClick(next);
-		}
+  }
 
-}
-
-setInterval((run),2000); //setInterval for my run() function to 2000ms
-
-void 0;
+setInterval(run, 6000);
