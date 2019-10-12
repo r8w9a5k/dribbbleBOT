30884103b3f621dfd46725146dab1dd00ed0db4d
Alice <47471302+A-Lice-404@users.noreply.github.com>
2019-10-12 15:58:44 +0300

    Update dribbbleBOT.js

--- dribbbleBOT.js
+++ dribbbleBOT.js
@@ -1,5 +1,5 @@
 function run() {
-  var like = document.querySelector(".shot-header .like-shot");
+  var like = document.querySelector('.shot-header .like-shot');
   var next = document.querySelector(".shot-nav-next a");
 
   function simulateClick(elem) {
