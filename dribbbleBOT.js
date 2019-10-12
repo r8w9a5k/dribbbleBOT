fdc41cd135ea2ee24cbd502eccb8b9d1805fada6
Alice <47471302+A-Lice-404@users.noreply.github.com>
2019-10-12 15:44:28 +0300

    Updates class name to valid one
    It turned out that old class selector didn't work.
    Also decreased the interval to make it a bit fater.

--- dribbbleBOT.js
+++ dribbbleBOT.js
@@ -1,26 +1,24 @@
 function run() {
-var like = document.querySelector(".shot-overlay .like-shot");
-var next = document.querySelector(".shot-nav-next a");
+  var like = document.querySelector(".shot-header .like-shot");
+  var next = document.querySelector(".shot-nav-next a");
 
-  function simulateClick (elem) {
-    var evt = new MouseEvent('click', {
+  function simulateClick(elem) {
+    var evt = new MouseEvent("click", {
       bubbles: true,
       cancelable: true,
-      view: window 
-      });
-      var canceled = !elem.dispatchEvent(evt);
-      };
+      view: window
+    });
+    var canceled = !elem.dispatchEvent(evt);
+  };
 
-      if (!like.classList.contains("current-user-likes")){
-	simulateClick(like);
-	simulateClick(next);
-	console.log("Liked!");
-      }
-      else {
-	simulateClick(next);
-	console.log("Skipped!");
-      }
-
+  if (!like.classList.contains("current-user-likes")) {
+    simulateClick(like);
+    simulateClick(next);
+    console.log("Liked!");
+  } else {
+    simulateClick(next);
+    console.log("Skipped!");
   }
+}
 
-setInterval(run, 6000);
+setInterval(run, 2000);
